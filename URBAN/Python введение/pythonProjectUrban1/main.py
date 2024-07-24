b57aa09784f774e2431adb59d8306cd626aada83
Ellepfan <Ellehp@yandex.ru>
2024-07-24 15:12:05 +0400

    pytonString

---
+++ URBAN/Python введение/pythonProjectUrban1/main.py
@@ -0,0 +1,21 @@
+# 1st program
+namber = (9**0.5)*5
+print("1st program -", (9**0.5)*5)
+
+# 2nd program
+print("2nd program -", 9.99 > 9.98 or 9.99 != 1000.1)
+
+# 3rd program
+namber1 = 1234
+namber2 = 5678
+sum = (namber1%1000//10) + (namber2%1000//10)
+print("3rd program - ", sum)
+
+# 4th program
+namber3 = 13.42
+namber4 = 42.13
+
+print("4th program - ", (namber3*100//100)==(namber4*100%100) or ((namber3*100%100)==(namber3*100//100)))
+
+
+
