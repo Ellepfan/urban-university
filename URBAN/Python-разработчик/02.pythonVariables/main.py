3581a8e1e09184b41227602437003d220f581216
Ellepfan <Ellehp@yandex.ru>
2024-07-25 12:45:52 +0400

    dz 02.pythonVariables

---
+++ URBAN/Python-разработчик/02.pythonVariables/main.py
@@ -0,0 +1,8 @@
+course = "Python"
+total_tasks = 12
+hours_spent = 1.5
+average_execution_time = hours_spent / total_tasks
+
+print("Курс: "+ course +", всего задач: " + str (total_tasks) +", затрачено часов:" + str(hours_spent) + ", среднее время выполнения", average_execution_time, "часа ")
+
+
