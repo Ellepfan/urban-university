f89bded16b159abd4396ff3a06d26e0e33424159
Ellepfan <Ellehp@yandex.ru>
2024-09-12 02:15:00 +0400

    06.2 Применить сокрытие атрибутов и повторить наследование. Рассмотреть на примере объекта из реального мира

---
+++ URBAN/Python-разработчик/06.ClassInheritance/02.pythonAccessParentProperties/main.py
@@ -0,0 +1,64 @@
+# В этой задаче мы реализуем классы транспорта, в которых нельзя будет просто
+# так поменять цвет, мощность двигателя и прочие свойства, т.к. в реальной жизни
+# это чаще всего делается не владельцем, а в специальных сервисах. Да, узнать значения
+# этих свойств мы сможем, но вот изменить - нет.
+#
+# Атрибут owner(str) - владелец транспорта. (владелец может меняться)
+# Атрибут __model(str) - модель (марка) транспорта.
+# (мы не можем менять название модели)
+# Атрибут __engine_power(int) - мощность двигателя.
+# (мы не можем менять мощность двигателя самостоятельно)
+# Атрибут __color(str) - название цвета.
+# (мы не можем менять цвет автомобиля своими руками)
+# Атрибут класса __COLOR_VARIANTS,
+# в который записан список допустимых цветов для окрашивания.
+class Vehicle:
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner: str, __model: str, __color: str,__engine_power: int):
+        self.owner = owner
+        self.__model = __model
+        self.__engine_power = __engine_power
+        self.__color = __color
+
+    def get_model(self):
+        return f"Модель: {self.__model}"
+
+    def get_horsepower(self):
+        return f"Мощность двигателя: {self.__engine_power}"
+
+    def get_color(self):
+        return f"Цвет: {self.__color}"
+
+    def print_info(self):
+        print(f"{self.get_model()}\n{self.get_horsepower()}\n{self.get_color()}\n"
+              f"Владелец: {self.owner}")
+
+    def set_color(self, new_color: str):
+        for color in self.__COLOR_VARIANTS:
+            if new_color.upper() == color.upper():
+                self.__color = new_color
+                break
+        else:
+            print(f"Нельзя сменить цвет на {new_color}")
+
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+    pass
+
+
+# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+
+# Изначальные свойства
+vehicle1.print_info()
+
+# Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+# Проверяем что поменялось
+vehicle1.print_info()
