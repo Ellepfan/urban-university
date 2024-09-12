81af23a91f7f190d87120a47889241e85b3abb09
Ellepfan <Ellehp@yandex.ru>
2024-09-12 15:15:23 +0400

    06.3 Множественное наследование

---
+++ URBAN/Python-разработчик/06.ClassInheritance/03.pythonMultipleInheritance/main.py
@@ -0,0 +1,57 @@
+# Создайте классы родители: Horse и Eagle с методами из описания.
+# Создайте класс наследник Pegasus с методами из описания.
+# Создайте объект класса Pegasus и вызовите каждый из ранее перечисленных методов,
+# проверив их работу.
+# x_distance - пройденный путь.
+# sound 'Frrr'
+# где dx - изменение дистанции
+# y_distance- высота полёта
+# sound  - звук, который издаёт орёл(отсылка)
+# dy - изменение дистанции
+
+class Horse:
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
+        super().__init__()
+
+    def run(self, dx):
+        self.x_distance += dx
+
+
+class Eagle:
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
+        super().__init__()
+
+
+
+    def fly(self, dy):
+        self.y_distance += dy
+
+
+class Pegasus(Horse, Eagle):
+    def __init__(self):
+        super().__init__()
+
+    def move(self, dx, dy):
+        self.run(dx)
+        self.fly(dy)
+
+    def get_pos(self):
+        return self.x_distance, self.y_distance
+
+    def voice(self):
+        print(self.sound)
+
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
