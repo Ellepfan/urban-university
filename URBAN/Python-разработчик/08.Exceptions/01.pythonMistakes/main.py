152b13d04185f7c5310f5eba6594345808441ed0
Ellepfan <ellehp@yandex.ru>
2024-09-29 20:16:06 +0400

    01.Try и Except

--- URBAN/Python-разработчик/08.Exceptions/01.pythonMistakes/main.py
+++ URBAN/Python-разработчик/08.Exceptions/01.pythonMistakes/main.py
@@ -1,9 +1,9 @@
 def add_everything_up(a, b):
     try:
         return a + b
-    except:
+
+    except TypeError:
         return str(a) + str(b)
-        pass
 
 
 print(add_everything_up(123.456, 'строка'))
