5491be55dfb879c64b676b207ec2784c2809da69
Ellepfan <ellehp@yandex.ru>
2024-11-18 20:05:26 +0400

    02.Юнит-Тесты

---
+++ URBAN/Python-разработчик/12.DevelopmentCycleSupport/02.pythonUnitTestingMethods/main.py
@@ -0,0 +1,50 @@
+import pprint
+import unittest
+from runner_and_tournament import Runner, Tournament
+
+
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.sportsman_1 = Runner("Усэйн", 10)
+        self.sportsman_2 = Runner("Андрей", 9)
+        self.sportsman_3 = Runner("Ник", 3)
+
+    def race1(self):
+        competitions1 = Tournament(90, self.sportsman_1, self.sportsman_3)
+        result1 = competitions1.start()
+        self.all_results['Тест первого раунда'] = result1
+        test = result1[len(result1)].name
+        self.assertTrue(test == "Ник")
+
+
+    def race2(self):
+        competitions2 = Tournament(90, self.sportsman_2, self.sportsman_3)
+        result2 = competitions2.start()
+        self.all_results['Тест второго раунда'] = result2
+        test = result2[len(result2)].name
+        self.assertTrue(test == "Ник")
+
+    def race3(self):
+        competitions3 = Tournament(90, self.sportsman_1, self.sportsman_2, self.sportsman_3)
+        result3 = competitions3.start()
+        self.all_results['Тест третьего раунда'] = result3
+        test = result3[len(result3)].name
+        self.assertTrue(test == "Ник")
+
+    @classmethod
+    def tearDownClass(cls):
+        pprint.pprint(cls.all_results)
+        pass
+        # for test_key, test_value in cls.all_results.items():
+        #     print(f'Тест: {test_key}')
+        #     for key, value in test_value.items():
+        #         print(f'\t{key}: {value.name}')
+
+if __name__ == '__main__':
+    unittest.main()
+
+
