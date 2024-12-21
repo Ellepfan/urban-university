f8b6b810d36684130f4e8c3a91e02fe510f008d3
Ellepfan <ellehp@yandex.ru>
2024-12-21 20:10:44 +0400

    16.1.2.3.4 fast API CRUD Запросы: Get, Post, Put Delete

--- URBAN/Python-разработчик/16.LibrariesWorkingResources/04.PythonCRUDRequests/main.py
+++ URBAN/Python-разработчик/16.LibrariesWorkingResources/04.PythonCRUDRequests/main.py
@@ -4,20 +4,26 @@ from typing import Annotated
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return f"Главная страница"
 
-@app.get("/user/admin")
-async def admin_user():
-    return f"Вы вошли как администратор"
+users = {'1': 'Имя: Example, возраст: 18'}
 
-@app.get("/user/{user_id}")
-async def user_id(user_id: int = Path(ge=0, le=100, description="Enter User ID", example= "1")):
-    return f"Вы вошли как пользователь № {user_id}"
+@app.get("/users")
+async def users_dict()->dict:
+    return users
 
-@app.get("/user/{username}/{age}")
-async def user(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example= "UrbanUser")],
-               age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24'")]):
-    return f"Информация о пользователе. Имя: {username}, Возраст {age}"
+@app.post("/user/{username}/{age}")
+async def add_users(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example= "UrbanUser")],
+               age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) ->str:
+    user_id = str(int(max(users,key=int))+1)
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"User {user_id} is registered"
 
+@app.put("/user/{user_id}/{username}/{age}")
+async def user_update(user_id: str, username: str, age:int)-> str:
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"he user {user_id} is updated"
+
+@app.delete("/user/{user_id}")
+async def user_del(user_id:str):
+    users.pop(user_id)
+    return f"User {user_id} has been deleted"
